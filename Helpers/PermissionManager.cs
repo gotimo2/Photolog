a83c45a8c3591b2482fd74deb731ab362a926506
gotimo2 <timost1402@hotmail.com>
2023-06-23 19:26:26 +0200

    camera opens

---
+++ Helpers/PermissionManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photolog.Helpers
+{
+    public static class PermissionManager
+    {
+        public async static Task<bool> getCameraPermissions()
+        {
+            var permissionLevel = await Permissions.CheckStatusAsync<Permissions.Camera>();
+
+            if (permissionLevel != PermissionStatus.Granted)
+            {
+                await Permissions.RequestAsync<Permissions.Camera>();
+
+                permissionLevel = await Permissions.CheckStatusAsync<Permissions.Camera>();
+            }
+
+            if (permissionLevel == PermissionStatus.Denied || permissionLevel == PermissionStatus.Disabled)
+            {
+                return false;
+            }
+
+            if (permissionLevel == PermissionStatus.Granted)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public async static Task<bool> getStorageWritePermissions()
+        {
+            
+
+            var permissionLevel = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
+
+
+            if (permissionLevel != PermissionStatus.Granted)
+            {
+                await Permissions.RequestAsync<Permissions.StorageWrite>();
+
+               
+                permissionLevel = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
+                Console.WriteLine("Permission status: " + permissionLevel);
+            }
+
+            if (permissionLevel == PermissionStatus.Denied || permissionLevel == PermissionStatus.Disabled)
+            {
+                return false;
+            }
+
+            if (permissionLevel == PermissionStatus.Granted)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public async static Task<bool> getStorageReadPermissions()
+        {
+
+
+            var permissionLevel = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+
+
+            if (permissionLevel != PermissionStatus.Granted)
+            {
+                await Permissions.RequestAsync<Permissions.StorageRead>();
+
+
+                permissionLevel = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+                Console.WriteLine("Permission status: " + permissionLevel);
+            }
+
+            if (permissionLevel == PermissionStatus.Denied || permissionLevel == PermissionStatus.Disabled)
+            {
+                return false;
+            }
+
+            if (permissionLevel == PermissionStatus.Granted)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
--- Page/TakePhotoPage.razor.cs
+++ Page/TakePhotoPage.razor.cs
@@ -1,12 +1,30 @@
-﻿namespace Photolog.Page
+﻿using Photolog.Helpers;
+
+namespace Photolog.Page
 {
     public partial class TakePhotoPage
     {
 
         protected override async Task OnInitializedAsync()
         {
+
             if (!MediaPicker.Default.IsCaptureSupported) {
                 throw new InvalidOperationException("This device cannot take a photo!");
+            }
+
+            if (await PermissionManager.getCameraPermissions() == false)
+            {
+                throw new InvalidOperationException("No camera permissions!");
+            }
+
+            if (await PermissionManager.getStorageReadPermissions() == false)
+            {
+                throw new InvalidOperationException("No storage read permissions!");
+            }
+            
+            if (await PermissionManager.getStorageWritePermissions() == false)
+            {
+                throw new InvalidOperationException("No storage write permissions!");
             }
 
             FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
